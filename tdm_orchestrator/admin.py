23de26c9ab66b44f235da651e22ca772d59ac135
edmond <edmond.y@h2altitude.com>
2026-02-09 14:28:14 +0100

    feat: creation des models et migrations

--- tdm_orchestrator/admin.py
+++ tdm_orchestrator/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Type, Entity, DataSource, Application, SqlScript, Runner, RunnerStep, ExecutionLog
+
+admin.site.register(Type)
+admin.site.register(Entity)
+admin.site.register(DataSource)
+admin.site.register(Application)
+admin.site.register(SqlScript)
+admin.site.register(Runner)
+admin.site.register(RunnerStep)
+admin.site.register(ExecutionLog)
